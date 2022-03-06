26abcd228cb56d1737338b4ea2596a43dca02b7b
grabowsky <grabowsky@grabowsky-ThinkPad-E15>
2022-03-06 15:50:44 +0100

    SoundPlayer

--- BestMindDemo/src/application/SoundPlayer.java
+++ BestMindDemo/src/application/SoundPlayer.java
@@ -11,15 +11,13 @@ import javax.sound.sampled.Mixer;
 public class SoundPlayer {
 	
 	
-
-   // System.out.println(String.format("Name [%s]\n", info.getName()));
-   // System.out.println(info.getDescription());
 	public static void play() {
 		try {
 	    	Mixer.Info[] mixerInfo = AudioSystem.getMixerInfo();
 	        Mixer.Info info = mixerInfo[2]; //Edit this number to select output // 0 = Default
 	        Clip clip = AudioSystem.getClip(info);
 	        clip.open(AudioSystem.getAudioInputStream(new File("/home/grabowsky/git/Resources/Sound/bgMusic.wav")));
+	        
 	        clip.start();
 	    } catch (Exception e) {
 	    	System.out.println(e);
